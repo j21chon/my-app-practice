317b455a2b6a9a10a1f86818df56d16de09532fe
Kim Jungho <j21chon@gmail.com>
2021-08-09 16:40:12 +0900

    client_fill

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
 import memories from './images/dance.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">FallinM</Typography>
-                <img src={memories} alt="memories" height="200" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">FallinM</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(express.json({ limit: "30mb", extended: true })) // body-parser 대신 express 내장 사용하면 됨 / 사진을 보내는데 그 용량을 제한한 것
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
@@ -21,6 +21,8 @@ app.use(cors());
 //         credentials: true
 //     })
 // )
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://fallinM:fallinM123@sandbox.exnqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
