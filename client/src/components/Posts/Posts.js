a9da6d0e55d691d140de406dc77edab5f9b851fc
Kim Jungho <j21chon@gmail.com>
2021-09-15 15:50:58 +0900

    propsdrilling

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -7,18 +7,18 @@ import useStyles from './styles'
 
 
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts)
     const classes = useStyles();
 
-    console.log(posts);
+    //console.log(posts);
     return (
         !posts.length ? <CircularProgres /> : (
             <Grid className={classes.container} container alignItems="stetch" spacing={3}>
                 {
                     posts.map((post) => (
                         <Grid key={post._id} item xs={12} sm={6}>
-                            <Post post={post} /> 
+                            <Post post={post} setCurrentId={setCurrentId} /> 
                         </Grid>
                     ))
                 }
