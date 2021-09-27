644748c11513d00fe5feae87307c0ea1f43c79c9
Kim Jungho <j21chon@gmail.com>
2021-09-27 14:32:35 +0900

    adjust

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,5 @@
 
-import React, { useStat, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64'
 import { useDispatch, useSelector } from 'react-redux'
